7a97539f66ad4b3247f228508a6474eec7422bb4
mkhmm17 <maxmelhi95@gmail.com>
2021-02-03 09:59:44 +0300

    Model

---
+++ Pryaniki/MainStruct.swift
@@ -0,0 +1,40 @@
+//
+//  MainStruct.swift
+//  Pryaniki
+//
+//  Created by muslim on 03.02.2021.
+//
+
+import Foundation
+
+// MARK: - Main
+struct Main: Codable {
+    let data: [Datum]
+    let view: [String]
+}
+
+// MARK: - Datum
+struct Datum: Codable {
+    let name: String
+    let data: DataClass
+}
+
+// MARK: - DataClass
+struct DataClass: Codable {
+    let text: String?
+    let url: String?
+    let selectedID: Int?
+    let variants: [Variant]?
+
+    enum CodingKeys: String, CodingKey {
+        case text, url
+        case selectedID = "selectedId"
+        case variants
+    }
+}
+
+// MARK: - Variant
+struct Variant: Codable {
+    let id: Int
+    let text: String
+}
